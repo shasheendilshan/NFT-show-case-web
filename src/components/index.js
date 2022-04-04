9674d2b40ba426ed257808e29068e8206a88786e
unknown <shasheen@i-threesixty.com>
2022-04-04 19:00:16 +0530

    inital commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Features from "./Features";
+import Download from "./Download";
+import Button from "./Button";
+import SectionWrapper from "./SectionWrapper";
+
+export { Features, Download, Button, SectionWrapper };
